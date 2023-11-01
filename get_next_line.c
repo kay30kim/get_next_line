6ad5d2033a69831233abccef3aad78ea70705fd5
kay30kim <kay30kim@gmail.com>
2023-11-01 17:40:41 -0400

    compare

--- get_next_line.c
+++ get_next_line.c
@@ -5,120 +5,164 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kyung-ki <kyung-ki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/30 11:06:42 by kyung-ki          #+#    #+#             */
-/*   Updated: 2023/10/31 18:24:30 by kyung-ki         ###   ########.fr       */
+/*   Created: 2023/11/01 13:38:12 by kyung-ki          #+#    #+#             */
+/*   Updated: 2023/11/01 17:39:56 by kyung-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char *ft_next(char **tmp)
+static char	*ft_next(char **temp)
 {
 	char	*line;
 	char	*ptr;
 
-	ptr = *tmp;
+	ptr = *temp;
 	while (*ptr && *ptr != '\n')
 		++ptr;
 	ptr += (*ptr == '\n');
-	line = ft_substr (*tmp, 0, (size_t)(ptr - *tmp));
+	line = ft_substr (*temp, 0, (size_t)(ptr - *temp));
 	if (!line)
 	{
-		free (*tmp);
+		free (*temp);
 		return (NULL);
 	}
 	ptr = ft_substr (ptr, 0, ft_strlen (ptr));
-	free (*tmp);
-	*tmp = ptr;
+	free (*temp);
+	*temp = ptr;
 	return (line);
 }
 
-// when read finish to read then return 0
-// static means that only can use in this source file
-static char *ft_read(char *tmp, int fd, char *buf)
+static char	*ft_read(char *temp, int fd, char *buf)
 {
 	ssize_t		r;
 
 	r = 1;
-	while (r && !ft_strchr (tmp, '\n'))
+	while (r && !ft_strchr (temp, '\n'))
 	{
 		r = read (fd, buf, BUFFER_SIZE);
 		if (r == -1)
 		{
-			free(buf)
+			free (buf);
+			free (temp);
 			return (NULL);
 		}
 		buf[r] = 0;
-		tmp = ft_strjoin_free(tmp, buf);
-		if (!tmp)
+		temp = ft_strjoin_free_s1 (temp, buf);
+		if (!temp)
 		{
 			free (buf);
 			return (NULL);
 		}
 	}
 	free (buf);
-	return (tmp);
+	return (temp);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*tmp[BUFFER_SIZE];
+	static char	*temp[OPEN_MAX];
 	char		*buf;
 
 	if (fd == -1 || BUFFER_SIZE < 1)
 		return (NULL);
-	if (!tmp[fd])
-		tmp[fd] = ft_strdup("");
-	if (!tmp[fd])
+	if (!temp[fd])
+		temp[fd] = ft_strdup("");
+	if (!temp[fd])
 		return (NULL);
-	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	buf = malloc (sizeof (*buf) * (BUFFER_SIZE + 1));
 	if (!buf)
 	{
-		free(tmp[fd]);
-		return(NULL);
-	}
-	tmp[fd] = ft_read (tmp[fd], fd, buf);
-	if (!tmp[fd])
+		free (temp[fd]);
 		return (NULL);
-	return (ft_next(&tmp[fd]));
+	}
+	temp[fd] = ft_read (temp[fd], fd, buf);
+	if (!temp[fd])
+		return (NULL);
+	if (!*temp[fd])
+	{
+		free (temp[fd]);
+		temp[fd] = NULL;
+		return (NULL);
+	}
+	return (ft_next(&temp[fd]));
 }
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd) {
-    static char buffer[BUFFER_SIZE];
-    static char line[LINE_SIZE];
-    static int index = 0;
-    static int line_length = 0;
-    int bytes_read;
+/*
 
-    while (1) {
-        if (index >= line_length) {
-            // The current line has been processed, read a new line
-            bytes_read = read(fd, buffer, BUFFER_SIZE);
-            if (bytes_read <= 0) {
-                // No more to read or an error occurred
-                if (line_length == 0) {
-                    return NULL; // Nothing to read or error
-                }
-                line[line_length] = '\0';
-                return line; // Return the last line
-            }
-            index = 0;
-            line_length = 0;
-        }
+static char	*read_line(int fd)
+{
+	char	buf[BUFFER_SIZE + 1];
+	char	*line;
+	int		bytes_read;
+	int		i;
 
-        while (index < bytes_read) {
-            if (buffer[index] == '\n') {
-                // End of line found
-                line[line_length] = '\0';
-                index++; // Move past '\n'
-                return line; // Return the line (including '\n')
-            } else {
-                line[line_length] = buffer[index];
-                index++;
-                line_length++;
-            }
-        }
-    }
+	bytes_read = read(fd, buf, BUFFER_SIZE);
+	if (bytes_read <= 0)
+		return (NULL);
+	buf[bytes_read] = '\0';
+	line = malloc(BUFFER_SIZE);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (i < bytes_read && buf[i] != '\n')
+	{
+		line[i] = buf[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
 }
+
+char	*get_next_line(int fd)
+{
+	static char	*remainder;
+	char		*line;
+	char		*tmp;
+	int			offset;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	line = NULL;
+    offset = 0;
+	while (!line)
+    {
+        if (!remainder)
+            tmp = ft_strdup("");
+        else
+            tmp = ft_strdup(remainder);
+        
+        free(remainder);
+        remainder = NULL;
+        
+        tmp = ft_strjoin(tmp, read_line(fd));
+        
+        if (!tmp || !tmp[0])
+        {
+            free(tmp);
+            return (NULL);
+        }
+        
+        while (tmp[offset] && tmp[offset] != '\n')
+            offset++;
+        
+        if (tmp[offset] == '\n')
+        {
+            tmp[offset] = '\0';
+            remainder = ft_strdup(tmp + offset + 1);
+        }
+        
+        if (line)
+            free(line);
+        line = tmp;
+        
+        if (tmp[offset] == '\n')
+            break;
+    }
+	return (line);
+}
+
+*/
+
--- get_next_line.h
+++ get_next_line.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kyung-ki <kyung-ki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/30 11:06:45 by kyung-ki          #+#    #+#             */
-/*   Updated: 2023/10/31 18:24:39 by kyung-ki         ###   ########.fr       */
+/*   Created: 2023/11/01 13:38:09 by kyung-ki          #+#    #+#             */
+/*   Updated: 2023/11/01 17:39:40 by kyung-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,39 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1024
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
+
+char	*ft_strchr(char const *s, int c);
+size_t	ft_strlen(char const *str);
+char	*ft_strdup(char const *s1);
+char	*ft_strjoin_free_s1(char *s1, char const *s2);
+char	*ft_substr(char const *str, unsigned int start, size_t size);
+char	*get_next_line(int fd);
 
 #endif
 
-
+/*
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 24
+#endif
 
-# define BUFFER_SIZE 42
-# define LINE_SIZE 4096
+#include <stdlib.h>
+#include <unistd.h>
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strdup(const char *s1);
 
 #endif
+*/
